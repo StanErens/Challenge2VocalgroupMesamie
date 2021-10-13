ca318fb3c3eb3271d851dba4de44cac0fd63320f
StanErens <406294@vistacollege.nl>
2021-10-13 11:35:28 +0200

    Zo lekker

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+
+echo "<!DOCTYPE html>";
+
+?>
+
+<html>
+
+    <header>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+
+    </header>
+
+    <body>
+        
+    </body>
+
+    <footer>
+
+    </footer>
+
+
+</html>
