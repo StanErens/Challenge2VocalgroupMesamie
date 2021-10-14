e185d71da3cace7ac6504889f5bc16645899d144
NiekVanDenBoorn <501918@vistacollege.nl>
2021-10-14 09:46:15 +0200

    navbar fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -8,18 +8,18 @@ echo "<!DOCTYPE html>";
 <div id="image"><img src="Vocalgroup_Mes_Amie_v01_DEF_KLEUR_CONTOUREN (1).png" width="17%"></div>
 <div id="navbar">
     <div class="navbar">
-        <a href="#home">Home</a>
+        <a href="index.php">Home</a>
         <div class="dropdown">
             <button class="dropbtn">Over Ons
               <i class="fa fa-caret-down"></i>
             </button>
             <div class="dropdown-content">
-              <a href="#">Algemeen</a>
-              <a href="#">Bestuur</a>
-              <a href="#">Dirigent</a>
-              <a href="#">Combo</a>
-              <a href="#">Lid Worden</a>
-              <a href="#">Repetitie</a>
+              <a href="overons/algemeen.php">Algemeen</a>
+              <a href="overons/bestuur.php">Bestuur</a>
+              <a href="overons/dirigent.php">Dirigent</a>
+              <a href="overons/combo.php">Combo</a>
+              <a href="overons/lidworden.php">Lid Worden</a>
+              <a href="overons/repetitie.php">Repetitie</a>
             </div>
           </div> 
         <div class="dropdown">
