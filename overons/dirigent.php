44eeb2914f2c592e4ce28678c28d4c6a74c7acd9
StanErens <406294@vistacollege.nl>
2021-10-22 12:18:54 +0200

    Informatie en foto's per pagina

--- overons/dirigent.php
+++ overons/dirigent.php
@@ -3,3 +3,16 @@
 echo "<!DOCTYPE html>";
 include_once("includes/navbar.php");
 ?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Dirigent</h1>
+        <p>Ons koor staat reeds meer dan 12,5 jaar onder leiding van Paul Lemmens.
+
+Met veel enthousiasme dirigeert hij niet alleen ons koor, indien onze vaste pianist niet beschikbaar is, begeleidt hij ons ook op de piano. Naast ons koor dirigeert Paul ook nog vier andere koren, waaronder ComboKoor Mozaik Hulsberg en Gemengd koor Schimmert. Overdag is hij te vinden op de basisscholen in de regio als muziekdocent. Ook keyboardles en pianoles zitten in zijn lesprogramma.</p>
+        <img src="images/koor_combo.jpg"></img>
+
+    </body>
+</html>
--- overons/repetitie.php
+++ overons/repetitie.php
@@ -3,3 +3,14 @@
 echo "<!DOCTYPE html>";
 include_once("includes/navbar.php");
 ?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Repetitie</h1>
+        <p>Ons koor repeteert op maandagavond van 19.30 – 21.30 uur in het Gemeenschapshuis De Amyerhoof, Severenplein 27, 6225 AZ Maastricht.</p>
+        <img src="images/amyerhoof.png"></img>
+
+    </body>
+</html>
