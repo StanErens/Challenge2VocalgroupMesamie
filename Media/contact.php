ef0320453c40ca68f931a24d8cf849116c5ceef2
StanErens <406294@vistacollege.nl>
2021-11-03 13:10:03 +0100

    Google Maps

--- Media/contact.php
+++ Media/contact.php
@@ -5,6 +5,7 @@ include_once("../includes/navbar.php");
 ?>
 
 <html>
+	<link rel="stylesheet" href="../css/style.css">
     <h1>Contact</h1>
 
     Repetitie-ruimte Amyerhoof 	 	Secretariaatadres 	 
@@ -12,6 +13,28 @@ include_once("../includes/navbar.php");
  	6225 AZ Maastricht	 	        6227 VT Maastricht	 
  	 	 	 	 
  	vocalgroupmesamie@gmail.com	 	 
+	 <div id="map"></div>
+	 <script>
+      // Initialize and add the map
+      function initMap() {
+       
+        const amby = { lat: 50.864300, lng: 5.731435 };
+        // The map, centered 
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: amby,
+        });
+        // The marker, positioned
+        const marker = new google.maps.Marker({
+          position: amby,
+          map: map,
+        });
+      }
+    </script>
+	<script
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
 
 
 </html>
